e13a47d788c55d8d2e4a96405fddc9527fcdb4ec
Jason Johnson <jvsonjohnson@gmail.com>
2020-02-12 11:00:02 -0500

    Contact html

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,14 @@
 from flask import Flask
+from flask_mail import Mail
+
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "staywoke"
+app.config["MAIL_SERVER"] = "smtp.mailtrap.io"
+app.config["MAIL_PORT"] = "465"  # (or try 2525)
+app.config["MAIL_USERNAME"] = "3ec7d02ea242c8"
+app.config["MAIL_PASSWORD"] = "731e330948a3d0"
+
+mail = Mail(app)
 from app import views
+
--- app/views.py
+++ app/views.py
@@ -7,22 +7,29 @@ This file creates your application.
 
 from app import app
 from flask import render_template, request, redirect, url_for, flash
-
+from forms import ContactForm
 
 ###
 # Routing for your application.
 ###
 
-@app.route('/')
+
+@app.route("/")
 def home():
     """Render website's home page."""
-    return render_template('home.html')
+    return render_template("home.html")
 
 
-@app.route('/about/')
+@app.route("/about/")
 def about():
     """Render the website's about page."""
-    return render_template('about.html', name="Mary Jane")
+    return render_template("about.html", name="Mary Jane")
+
+
+@app.route("/contact")
+def contact():
+    form = ContactForm
+    return render_template("contact.html")
 
 
 ###
@@ -34,16 +41,17 @@ def about():
 def flash_errors(form):
     for field, errors in form.errors.items():
         for error in errors:
-            flash(u"Error in the %s field - %s" % (
-                getattr(form, field).label.text,
-                error
-            ), 'danger')
+            flash(
+                u"Error in the %s field - %s"
+                % (getattr(form, field).label.text, error),
+                "danger",
+            )
 
 
-@app.route('/<file_name>.txt')
+@app.route("/<file_name>.txt")
 def send_text_file(file_name):
     """Send your static text file."""
-    file_dot_text = file_name + '.txt'
+    file_dot_text = file_name + ".txt"
     return app.send_static_file(file_dot_text)
 
 
@@ -54,16 +62,16 @@ def add_header(response):
     and also tell the browser not to cache the rendered page. If we wanted
     to we could change max-age to 600 seconds which would be 10 minutes.
     """
-    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
-    response.headers['Cache-Control'] = 'public, max-age=0'
+    response.headers["X-UA-Compatible"] = "IE=Edge,chrome=1"
+    response.headers["Cache-Control"] = "public, max-age=0"
     return response
 
 
 @app.errorhandler(404)
 def page_not_found(error):
     """Custom 404 page."""
-    return render_template('404.html'), 404
+    return render_template("404.html"), 404
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port="8080")
